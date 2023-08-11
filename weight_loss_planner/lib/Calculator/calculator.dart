9be5e6ed58776b17dd6f55da91642283c28f4534
Harrison Schuch <harry@myschuch.com>
2023-08-11 23:35:00 +1000

    Result Navigation and Styling
    - After entering all fields, it navigates to the results page which currently just displays the information parsed
    - Added new function to remove bloat in the widgets
    - Fixed the styling after calculating and keypad overflowing data

--- weight_loss_planner/lib/Calculator/calculator.dart
+++ weight_loss_planner/lib/Calculator/calculator.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:weight_loss_planner/Calculator/bmr_calculator.dart';
+import 'package:weight_loss_planner/Calculator/result.dart';
 import 'package:weight_loss_planner/sidebar.dart';
 
 // The Calculator Class Page
@@ -10,9 +11,27 @@ class Calculator extends StatelessWidget {
   Calculator({super.key});
   final _formkey = GlobalKey<FormBuilderState>();
 
+  /// Navigates to the result page when the form is filled
+  void navigateToResult(BuildContext context) {
+    if (_formkey.currentState?.saveAndValidate() == true) {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => Result(
+                  weight: int.parse(_formkey.currentState?.value['weight']),
+                  height: int.parse(_formkey.currentState?.value['height']),
+                  age: int.parse(_formkey.currentState?.value['age']),
+                  gender: _formkey.currentState?.value['gender'],
+                  activity: _formkey.currentState?.value['activity'])
+          )
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: Colors.blue[900],
         title: Text('Calorie Calculator'),
@@ -22,11 +41,9 @@ class Calculator extends StatelessWidget {
       drawerEnableOpenDragGesture: false,
       body: FormBuilder(
         key: _formkey,
-        // TODO: Get the styling working to avoid the pixel overflow
         child: Column(
           children: [
-            /// This will contain all the contents of the form
-            /// except for the buttons
+            /// This will contain all the contents for the form
             Padding(
               padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
               child: Container(
@@ -49,6 +66,8 @@ class Calculator extends StatelessWidget {
                     ),
                     SizedBox(height: 10)
                   ],
+
+                  /// Gender Radio Button Selection
                   FormBuilderRadioGroup(
                     name: 'gender',
                     decoration: InputDecoration(
@@ -63,6 +82,8 @@ class Calculator extends StatelessWidget {
                     ],
                   ),
                   SizedBox(height: 10),
+
+                  /// The Activity Drop Down Selection
                   FormBuilderDropdown(
                     name: 'activity',
                     decoration: InputDecoration(
@@ -81,13 +102,14 @@ class Calculator extends StatelessWidget {
                 ]),
               ),
             ),
-        
+
             /// The buttons at the bottom of the form
             Expanded(
               child: Container(
                 alignment: Alignment.bottomCenter,
                 child: Row(
                   children: [
+                    /// The Calculate Button
                     Expanded(
                         child: ElevatedButton(
                             style: ElevatedButton.styleFrom(
@@ -101,12 +123,11 @@ class Calculator extends StatelessWidget {
                                 ),
                               ),
                             ),
-                            onPressed: () {
-                              _formkey.currentState?.saveAndValidate();
-                              debugPrint(_formkey.currentState?.value.toString());
-                            },
+                            onPressed: () => navigateToResult(context),
                             child: Text('Calculate',
-                                style: TextStyle(fontSize: 24)))),
+style: TextStyle(fontSize: 24)))),
+
+                    /// The Reset Button
                     Expanded(
                       child: ElevatedButton(
                           style: ElevatedButton.styleFrom(
@@ -120,9 +141,7 @@ class Calculator extends StatelessWidget {
                               ),
                             ),
                           ),
-                          onPressed: () {
-                            _formkey.currentState?.reset();
-                          },
+                          onPressed: () => _formkey.currentState?.reset(),
                           child: Text('Reset', style: TextStyle(fontSize: 24))),
                     )
                   ],
